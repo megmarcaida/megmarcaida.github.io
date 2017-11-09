4b01a92430a7c057dd6d72a7065de05808073c97
megmarcaida <marcaidameg@gmail.com>
2017-11-10 03:15:41 +0800

    Commit NG

---
+++ app.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Latte on 11/10/2017.
+ */
+var latteApp = angular.module('latteApp', ['ngRoute', 'ngAnimate']);
+
+latteApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+        .when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutController'
+        })
+        .when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'contactController'
+        });
+
+});
+
+latteApp.controller('mainController', function($scope) {
+    $scope.pageClass = 'page-home';
+});
+
+latteApp.controller('aboutController', function($scope) {
+    $scope.pageClass = 'page-about';
+});
+
+latteApp.controller('contactController', function($scope) {
+    $scope.pageClass = 'page-contact';
+});
